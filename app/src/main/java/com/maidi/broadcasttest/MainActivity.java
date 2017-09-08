58b910eb803b5b263a3ff64802987afd560da1b0
huangkc <huangkaichun@maiditech.com>
2017-09-08 10:08:34 +0800

    动态注册广播需要在onDestroy取消注册，监听网络变化，有无网络

--- app/src/main/java/com/maidi/broadcasttest/MainActivity.java
+++ app/src/main/java/com/maidi/broadcasttest/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -33,6 +35,16 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onReceive(Context context, Intent intent) {
+            ConnectivityManager mConnectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo mNetworkInfo = mConnectivityManager.getActiveNetworkInfo();
+            if (mNetworkInfo != null && mNetworkInfo.isAvailable()) {
+                Toast.makeText(context, "network is available",
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(context, "network is unavailable",
+                        Toast.LENGTH_SHORT).show();
+            }
+
             Toast.makeText(context,"network changes",Toast.LENGTH_SHORT).show();
         }
     }
